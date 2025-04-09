eac9e9a206cce0eb8845e8192ade9da1a5d5da2f
berru <berruhanedar@icloud.com>
2025-04-09 14:24:43 +0300

    initial commit

---
+++ src/main/java/com/berru/app/elearningmanagementsystem/ELearningManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.berru.app.elearningmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELearningManagementSystemApplication.class, args);
+    }
+
+}
