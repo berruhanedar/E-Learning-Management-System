88ed197250d2f563984da4d4a55195c6fded707d
berru <berruhanedar@icloud.com>
2025-04-21 15:35:56 +0300

    updated

--- src/main/java/com/berru/app/elearningmanagementsystem/facade/BookingFacade.java
+++ src/main/java/com/berru/app/elearningmanagementsystem/facade/BookingFacade.java
@@ -44,56 +44,56 @@ public class BookingFacade {
 
     public ResponseEntity<CommonApiResponse> addBooking(NewBookingRequestDto request) {
 
-        LOG.info("request received for adding customer booking course");
+        LOG.info("Received request for adding a booking for the customer");
 
-        CommonApiResponse response = new CommonApiResponse();
+        CommonApiResponse apiResponse = new CommonApiResponse();
 
         String bookingTime = String
                 .valueOf(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
 
         if (request == null) {
-            response.setResponseMessage("missing input");
-            response.setSuccess(false);
+            apiResponse.setResponseMessage("Input data is missing");
+            apiResponse.setSuccess(false);
 
-            return new ResponseEntity<CommonApiResponse>(response, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<CommonApiResponse>(apiResponse, HttpStatus.BAD_REQUEST);
         }
 
         if (request.getCourseId() == 0 || request.getCustomerId() == 0 || request.getCvv() == null
                 || request.getExpiryDate() == null || request.getNameOnCard() == null || request.getCardNo() == null
                 || request.getAmount() == null) {
 
-            response.setResponseMessage("missing input or invalid details");
-            response.setSuccess(false);
+            apiResponse.setResponseMessage("Input data is missing or contains invalid details");
+            apiResponse.setSuccess(false);
 
-            return new ResponseEntity<CommonApiResponse>(response, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<CommonApiResponse>(apiResponse, HttpStatus.BAD_REQUEST);
 
         }
 
         User customer = this.userService.getUserById(request.getCustomerId());
 
         if (customer == null) {
-            response.setResponseMessage("customer not found!!!");
-            response.setSuccess(false);
+            apiResponse.setResponseMessage("Customer not found!!!");
+            apiResponse.setSuccess(false);
 
-            return new ResponseEntity<CommonApiResponse>(response, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<CommonApiResponse>(apiResponse, HttpStatus.BAD_REQUEST);
         }
 
         Course course = this.courseService.getById(request.getCourseId());
 
         if (course == null) {
-            response.setResponseMessage("course not found!!!");
-            response.setSuccess(false);
+            apiResponse.setResponseMessage("Course not found!!!");
+            apiResponse.setSuccess(false);
 
-            return new ResponseEntity<CommonApiResponse>(response, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<CommonApiResponse>(apiResponse, HttpStatus.BAD_REQUEST);
         }
 
         List<Booking> bookings = this.bookingService.getByCourseAndCustomer(course, customer);
 
         if (!CollectionUtils.isEmpty(bookings)) {
-            response.setResponseMessage("Course Already Purchased!!!");
-            response.setSuccess(false);
+            apiResponse.setResponseMessage("Course Has Already Been Purchased!!!");
+            apiResponse.setSuccess(false);
 
-            return new ResponseEntity<CommonApiResponse>(response, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<CommonApiResponse>(apiResponse, HttpStatus.BAD_REQUEST);
         }
 
         String bookingId = BookingIdGenerator.generateTourBookingId();
@@ -112,10 +112,10 @@ public class BookingFacade {
         Payment savedPayment = this.paymentService.addPayment(payment);
 
         if (savedPayment == null) {
-            response.setResponseMessage("Failed to Purchase the Course, Payment Failure!!!");
-            response.setSuccess(false);
+            apiResponse.setResponseMessage("Payment failed, course purchase unsuccessful.");
+            apiResponse.setSuccess(false);
 
-            return new ResponseEntity<CommonApiResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<CommonApiResponse>(apiResponse, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         User mentor = course.getMentor();
@@ -135,16 +135,16 @@ public class BookingFacade {
         Booking savedBooking = this.bookingService.addBooking(booking);
 
         if (savedBooking == null) {
-            response.setResponseMessage("Failed to Book Event, Internal Error");
-            response.setSuccess(false);
+            apiResponse.setResponseMessage("Event booking failed due to internal error.");
+            apiResponse.setSuccess(false);
 
-            return new ResponseEntity<CommonApiResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<CommonApiResponse>(apiResponse, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        response.setResponseMessage("Congralutions, Course Purchased!!!");
-        response.setSuccess(true);
+        apiResponse.setResponseMessage("Congratulations, Course Purchased successfully.");
+        apiResponse.setSuccess(true);
 
-        return new ResponseEntity<CommonApiResponse>(response, HttpStatus.OK);
+        return new ResponseEntity<CommonApiResponse>(apiResponse, HttpStatus.OK);
 
     }
 
@@ -155,14 +155,14 @@ public class BookingFacade {
         List<Booking> bookings = this.bookingService.getAllBookings();
 
         if (CollectionUtils.isEmpty(bookings)) {
-            response.setResponseMessage("No Course Purchases found!!");
+            response.setResponseMessage("No course purchases found.");
             response.setSuccess(false);
 
             return new ResponseEntity<BookingResponseDto>(response, HttpStatus.BAD_REQUEST);
         }
 
         response.setBookings(bookings);
-        response.setResponseMessage("Fetched all Course Purchases!!!");
+        response.setResponseMessage("Successfully fetched all course purchases.");
         response.setSuccess(true);
 
         return new ResponseEntity<BookingResponseDto>(response, HttpStatus.OK);
@@ -173,7 +173,7 @@ public class BookingFacade {
         BookingResponseDto response = new BookingResponseDto();
 
         if (courseId == null) {
-            response.setResponseMessage("missing input");
+            response.setResponseMessage("Course ID is missing.");
             response.setSuccess(false);
 
             return new ResponseEntity<BookingResponseDto>(response, HttpStatus.BAD_REQUEST);
@@ -183,7 +183,7 @@ public class BookingFacade {
         Course course = this.courseService.getById(courseId);
 
         if (course == null) {
-            response.setResponseMessage("course not found!!!");
+            response.setResponseMessage("Course not found.");
             response.setSuccess(false);
 
             return new ResponseEntity<BookingResponseDto>(response, HttpStatus.BAD_REQUEST);
@@ -192,14 +192,14 @@ public class BookingFacade {
         List<Booking> bookings = this.bookingService.getByCourse(course);
 
         if (CollectionUtils.isEmpty(bookings)) {
-            response.setResponseMessage("No Course Purchases found!!");
+            response.setResponseMessage("No course purchases found.");
             response.setSuccess(false);
 
             return new ResponseEntity<BookingResponseDto>(response, HttpStatus.OK);
         }
 
         response.setBookings(bookings);
-        response.setResponseMessage("Fetched all Course Purchases!!!");
+        response.setResponseMessage("Successfully fetched all course purchases.");
         response.setSuccess(true);
 
         return new ResponseEntity<BookingResponseDto>(response, HttpStatus.OK);
@@ -210,7 +210,7 @@ public class BookingFacade {
         BookingResponseDto response = new BookingResponseDto();
 
         if (customerId == null) {
-            response.setResponseMessage("missing input");
+            response.setResponseMessage("Customer ID is missing.");
             response.setSuccess(false);
 
             return new ResponseEntity<BookingResponseDto>(response, HttpStatus.BAD_REQUEST);
@@ -220,7 +220,7 @@ public class BookingFacade {
         User customer = this.userService.getUserById(customerId);
 
         if (customer == null) {
-            response.setResponseMessage("customer not found!!!");
+            response.setResponseMessage("Customer not found.");
             response.setSuccess(false);
 
             return new ResponseEntity<BookingResponseDto>(response, HttpStatus.BAD_REQUEST);
@@ -229,14 +229,14 @@ public class BookingFacade {
         List<Booking> bookings = this.bookingService.getBookingByCustomer(customer);
 
         if (CollectionUtils.isEmpty(bookings)) {
-            response.setResponseMessage("No Course Purchases found");
+            response.setResponseMessage("No Course Purchases found.");
             response.setSuccess(false);
 
             return new ResponseEntity<BookingResponseDto>(response, HttpStatus.OK);
         }
 
         response.setBookings(bookings);
-        response.setResponseMessage("Fetched Course Purchases!!");
+        response.setResponseMessage("Course Purchases fetched successfully.");
         response.setSuccess(true);
 
         return new ResponseEntity<BookingResponseDto>(response, HttpStatus.OK);
@@ -247,7 +247,7 @@ public class BookingFacade {
         BookingResponseDto response = new BookingResponseDto();
 
         if (mentorId == null) {
-            response.setResponseMessage("missing input");
+            response.setResponseMessage("Missing input");
             response.setSuccess(false);
 
             return new ResponseEntity<BookingResponseDto>(response, HttpStatus.BAD_REQUEST);
@@ -257,7 +257,7 @@ public class BookingFacade {
         User mentor = this.userService.getUserById(mentorId);
 
         if (mentor == null) {
-            response.setResponseMessage("mentor not found!!!");
+            response.setResponseMessage("Mentor not found!!!");
             response.setSuccess(false);
 
             return new ResponseEntity<BookingResponseDto>(response, HttpStatus.BAD_REQUEST);
@@ -266,14 +266,14 @@ public class BookingFacade {
         List<Booking> bookings = this.bookingService.getByMentor(mentor);
 
         if (CollectionUtils.isEmpty(bookings)) {
-            response.setResponseMessage("No Course Purchases found");
+            response.setResponseMessage("No course purchases found");
             response.setSuccess(false);
 
             return new ResponseEntity<BookingResponseDto>(response, HttpStatus.OK);
         }
 
         response.setBookings(bookings);
-        response.setResponseMessage("Fetched all Course Purchases!!");
+        response.setResponseMessage("Fetched all course purchases!!");
         response.setSuccess(true);
 
         return new ResponseEntity<BookingResponseDto>(response, HttpStatus.OK);
